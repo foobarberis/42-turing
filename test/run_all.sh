eab60426ec0aeb5a6346c6af4aa11ea2602254f4
Matthieu Barberis <foobarberis@gmail.com>
2026-05-13 16:46:18 +0200

    Misc (#5)
    * Implement basic project structure
    
    * Implement domain types
    
    * Implement parsing to transform a JSON file into an OCaml 'machine' object
    
    * Implement unit tests for parsing
    
    * Write documentation on parsing
    
    * Implement validation
    
    * Implement validation tests
    
    * Implement validation documentation
    
    * Update documentation
    
    * Delete .keep
    
    * Finish main () implementation
    
    * Create end to end test run
    
    * Update README
    
    Cleanup
    Add section on how a Turing machine work
    Add section on functional programming
    
    * Cleanup machines

--- test/run_all.sh
+++ test/run_all.sh
@@ -5,20 +5,30 @@ cd "$(dirname "$0")/.."
 
 LOG="test/log.txt"
 : > "$LOG"
+count=0
 
 run() {
 	json=$1
 	input=$2
+	printf '[e2e] %s "%s"\n' "$json" "$input"
 	printf './ft_turing %s "%s"\n' "$json" "$input" >> "$LOG"
-	./ft_turing "$json" "$input" >> "$LOG" 2>&1
-	printf '\n\n' >> "$LOG"
+	if ./ft_turing "$json" "$input" >> "$LOG" 2>&1; then
+		printf 'OK\n\n' >> "$LOG"
+		count=$((count + 1))
+	else
+		printf 'FAIL: %s "%s"\n' "$json" "$input" >&2
+		exit 1
+	fi
 }
 
-run "res/02n.json" "000111"
+run "res/02n.json" ""
+run "res/02n.json" "0000"
+run "res/0n1n.json" ""
 run "res/0n1n.json" "000111"
-run "res/infloop.json" "1......0"
+run "res/is_palindrome.json" ""
 run "res/is_palindrome.json" "1000000001"
-run "res/unary_add.json" "1+1="
-run "res/unary-add-min.json" "1+1="
-run "res/unary_sub.json" "111-11="
+run "res/unary_add.json" "11+1111="
 run "res/utm.json" "1.+=|.|ABCDE|A|E|A.A.RA1A1RA+A+RA=B.LB1C=LB+E.LC1C1LC+D1RD1D1RD=E.R|_11+1111="
+
+printf 'OK: %d e2e cases passed\n' "$count"
+printf 'OK: %d e2e cases passed\n' "$count" >> "$LOG"
