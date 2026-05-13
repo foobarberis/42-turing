4e717a830dff9be5bece77867eed43cbba4da1a8
Emma <emmatele03@gmail.com>
2026-05-13 19:23:46 +0200

    * add my Own type on Types.ml

--- src/types.ml
+++ src/types.ml
@@ -1,20 +1,33 @@
 type action = 
-  | Left
-  | Right
+	| Left
+	| Right
 
 type transition = {
-  read : char;
-  to_state : string;
-  write : char;
-  action : action;
+	read : char;
+	to_state : string;
+	write : char;
+	action : action;
 }
 
 type machine = {
-  name : string;
-  alphabet : char list;
-  blank : char;
-  states : string list;
-  initial : string;
-  finals : string list;
-  transitions : (string * transition list) list;
+	name : string;
+	alphabet : char list;
+	blank : char;
+	states : string list;
+	initial : string;
+	finals : string list;
+	transitions : (string * transition list) list;
 }
+
+(* Tape representation *)
+type tape = {
+	left : char list;			(* Symbols to the left of the head, in reverse order *)
+	current : char;				(* Symbol under the head *)
+	right : char list;		(* Symbols to the right of the head *)
+}
+
+(* Execution result variant types *)
+type exec_result =
+  | Halted of tape
+  | Blocked of string * tape
+  | Continue of string * tape * transition
